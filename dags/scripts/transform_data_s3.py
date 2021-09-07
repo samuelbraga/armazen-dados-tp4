0c926b3b8ea6edba8118e3f07c8a561cc4018185
Daniel Miranda <danielvcmiranda@gmail.com>
2021-09-06 22:16:04 -0300

    Holidays integrated to dag

--- dags/scripts/transform_data_s3.py
+++ dags/scripts/transform_data_s3.py
@@ -3,16 +3,23 @@ import sys
 import pandas as pd
 from dateutil.parser import parse
 from datetime import datetime, timedelta
+from scripts.utils.holidays import Holidays
 
 from scripts.utils.s3 import get_file
 
 def transform_data(bucket_name, file_key):
     df = get_data_frame(bucket_name, file_key)
     create_columns(df)
-    for index, row in df.iterrows():
-        broke_date(df, index)
-    write_csv(df)
+    try:
+        year = get_year_from_file_key(file_key)
+    except ValueError:
+        raise Exception('unable to get year')
     
+    holiday_data = Holidays(year)
+    for index, row in df.iterrows():
+        broke_date(df, index, holiday_data)
+    write_csv(df)
+
 def get_data_frame(bucket_name, file_key):
     csv = get_file(bucket_name, file_key)
     body = csv['Body']
@@ -28,8 +35,9 @@ def create_columns(df):
     df["hour"] = ""
     df["day_of_week"] = ""
     df["shift"] = ""
+    df["is_holiday"] = ""
 
-def broke_date(df, index):
+def broke_date(df, index, holiday_data):
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     pickup_date = parse(df.at[index, 'tpep_pickup_datetime'])
     
@@ -38,7 +46,7 @@ def broke_date(df, index):
     df.at[index, 'year'] = pickup_date.year
     df.at[index, 'hour'] = pickup_date.hour
     df.at[index, 'day_of_week'] = days[pickup_date.weekday()]
-
+    df.at[index, 'is_holiday'] = holiday_data.is_holiday(pickup_date)
     if pickup_date.hour >= 4 and pickup_date.hour < 12:
         df.at[index, 'shift'] = "MORNING"
     if pickup_date.hour >= 12 and pickup_date.hour < 18:
@@ -50,3 +58,9 @@ def write_csv(data, filepath='./dags/tmp/'):
     filename = 'teste' + datetime.now().strftime('%Y_%m_%d_%H_%M_%S')+'.csv'
     filepath = filepath + filename
     data.to_csv(filepath, index_label='id')
+
+def get_year_from_file_key(file_key):
+    file_name_parts = file_key.split('_')
+    year_month = file_name_parts[-1].split('-')
+    year = int(year_month[0])
+    return year
--- dags/scripts/utils/holidays.py
+++ dags/scripts/utils/holidays.py
@@ -1,18 +1,20 @@
-import urllib3
+import requests
+from requests import Response
 import json
 from datetime import datetime
 class Holidays:
     def __init__(self, year):
         self.year = year
-        self.get_holidays()
+        self.holidays_dates = self.get_holidays()
     
     def get_holidays(self):
         api = PublicHolydaysApi()
         holidays_dicts = api.get_holidays_for_year(self.year)
-        self.holidays_dates = []
+        holidays_dates = []
         for holiday_dict in holidays_dicts:
             holiday = datetime.strptime(holiday_dict['date'], '%Y-%m-%d')
-            self.holidays_dates.append(holiday.date())
+            holidays_dates.append(holiday.date())
+        return holidays_dates
     
     def is_holiday(self, date: datetime):
         if date.date() in self.holidays_dates:
@@ -26,12 +28,11 @@ class PublicHolydaysApi:
         self.base_url = f'https://date.nager.at/api/{version}/publicholidays'
     
     def get_holidays_for_year(self, year, country_code='US', countie_code='NY'):
-        http = urllib3.PoolManager()
         request_url = self.base_url+f'/{year}/{country_code}'
-        response = http.request('GET', request_url)
-        if response.status != 200:
+        response = requests.get(request_url)
+        if not response.ok:
             raise Exception("Something went wrong with the Holidays API")
-        response = json.loads(response.data)
+        response = response.json()
         if countie_code != None:
             holidays = []
             for holiday in response:
